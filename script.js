0876b2291cd4763d177f5e966b22a480a7803a87
xbiotech <biotech9261@gmail.com>
2023-05-18 21:42:04 +0200

    feat: Add loop that creates table row with user input and delete button
    a loop was added that iterates through the array that stores the user input as objects and creates a table row displaying those user inputs including a delete button for the newly created table row

--- script.js
+++ script.js
@@ -27,7 +27,44 @@ function addBookToLibrary(e) {
   authorInput.value = "";
   pagesInput.value = "";
   readstatusInput.checked = false;
+
+  const table = document.getElementsByTagName("table")[0];
+  const tableBody = document.getElementsByTagName("tbody")[0];
+
+  for (let i = 0; i < library.length; i++) {
+    const object = library[i];
+
+    const tableRow = document.createElement("tr");
+
+    const titleCell = document.createElement("td");
+    titleCell.textContent = object.title;
+    tableRow.appendChild(titleCell);
+
+    const authorCell = document.createElement("td");
+    authorCell.textContent = object.author;
+    tableRow.appendChild(authorCell);
+
+    const pagesCell = document.createElement("td");
+    pagesCell.textContent = object.pages;
+    tableRow.appendChild(pagesCell);
+
+    const readstatusCell = document.createElement("td");
+    readstatusCell.textContent = object.readstatus;
+    tableRow.appendChild(readstatusCell);
+
+    const remove = document.createElement("td");
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-button");
+    removeBtn.textContent = "Delete";
+    tableRow.appendChild(remove);
+    remove.appendChild(removeBtn);
+    removeBtn.addEventListener("click", () => {
+      tableRow.remove();
+    });
+
+    tableBody.appendChild(tableRow);
+  }
+  table.appendChild(tableBody);
 }
 const submitButton = document.querySelector(".add-button");
 submitButton.addEventListener("click", addBookToLibrary);
-console.log(library);
