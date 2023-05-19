7667728c04b4f7fbb4a898a6ab5a8141208a805d
xBioTech <biotech9261@gmail.com>
2023-05-19 09:24:33 +0200

    fix: Fix bugs where books were not added and deleted correctly

--- script.js
+++ script.js
@@ -28,43 +28,69 @@ function addBookToLibrary(e) {
   pagesInput.value = "";
   readstatusInput.checked = false;
 
+  updateTable();
+}
+
+function addBookToTable(object, index) {
   const table = document.getElementsByTagName("table")[0];
   const tableBody = document.getElementsByTagName("tbody")[0];
 
+  const tableRow = document.createElement("tr");
+  tableRow.setAttribute("data-index", index);
+
+  const titleCell = document.createElement("td");
+  titleCell.textContent = object.title;
+  tableRow.appendChild(titleCell);
+
+  const authorCell = document.createElement("td");
+  authorCell.textContent = object.author;
+  tableRow.appendChild(authorCell);
+
+  const pagesCell = document.createElement("td");
+  pagesCell.textContent = object.pages;
+  tableRow.appendChild(pagesCell);
+
+  const readstatusCell = document.createElement("td");
+  readstatusCell.textContent = object.readstatus;
+  tableRow.appendChild(readstatusCell);
+
+  const remove = document.createElement("td");
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-button");
+  removeBtn.textContent = "Delete";
+  tableRow.appendChild(remove);
+  remove.appendChild(removeBtn);
+  removeBtn.addEventListener("click", () => {
+    const index = parseInt(tableRow.getAttribute("data-index"));
+    removeBookFromLibrary(index);
+  });
+
+  tableBody.appendChild(tableRow);
+  table.appendChild(tableBody);
+}
+
+function removeBookFromLibrary(index) {
+  const tableRow = document.querySelector(`tr[data-index="${index}"]`);
+  tableRow.remove();
+  library.splice(index, 1);
+  updateTableIndices();
+}
+
+function updateTable() {
+  const tableBody = document.getElementsByTagName("tbody")[0];
+  tableBody.innerHTML = "";
+
   for (let i = 0; i < library.length; i++) {
     const object = library[i];
-
-    const tableRow = document.createElement("tr");
-
-    const titleCell = document.createElement("td");
-    titleCell.textContent = object.title;
-    tableRow.appendChild(titleCell);
-
-    const authorCell = document.createElement("td");
-    authorCell.textContent = object.author;
-    tableRow.appendChild(authorCell);
-
-    const pagesCell = document.createElement("td");
-    pagesCell.textContent = object.pages;
-    tableRow.appendChild(pagesCell);
-
-    const readstatusCell = document.createElement("td");
-    readstatusCell.textContent = object.readstatus;
-    tableRow.appendChild(readstatusCell);
-
-    const remove = document.createElement("td");
-    const removeBtn = document.createElement("button");
-    removeBtn.classList.add("remove-button");
-    removeBtn.textContent = "Delete";
-    tableRow.appendChild(remove);
-    remove.appendChild(removeBtn);
-    removeBtn.addEventListener("click", () => {
-      tableRow.remove();
-    });
-
-    tableBody.appendChild(tableRow);
+    addBookToTable(object, i);
   }
-  table.appendChild(tableBody);
+}
+
+function updateTableIndices() {
+  const tableRows = document.querySelectorAll("tr[data-index]");
+  tableRows.forEach((row, index) => {
+    row.setAttribute("data-index", index);
+  });
 }
 const submitButton = document.querySelector(".add-button");
 submitButton.addEventListener("click", addBookToLibrary);
